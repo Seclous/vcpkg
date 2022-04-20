b84366b1beeff898058d8a7fd9cdb6737c2fcb67
Hannah Sauermann <hannah.sauermann@seclous.com>
2022-04-20 17:07:28 +0200

    [mailio] Use our custom mail version (v21.1)

--- ports/mailio/portfile.cmake
+++ ports/mailio/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO karastojko/mailio
-    REF cc887a7808d9c55e07c8a7503c7ae2e2d7485120 # version_0-21-0
-    SHA512 7125bfe4274e1e126e335b2e4b5743ef54d5dc0b6fd83f0c10e7578b57924d3e398af6b3865fdee3de587e2e2d7c33d95dbe017b1966649e68cf52f2dd268ee5
+    REPO Seclous/mailio
+    REF 48f5dc0ed50d400cd63e6f3d394390c4ba1a5036 # version_0-21-1
+    SHA512 49d2986bc2b0f1fcce6505e3358ef5808f9f7280a9aac70985cec0c5e5adce05e171dd78891d8dae7dc235759767ae0a29ea88c45669552948d387b79ddbb243
     HEAD_REF master
 )
 
